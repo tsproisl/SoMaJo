4d9a7b8f1ed640b3d3e38c068377c5e8c82ab3e7
Thomas Proisl <thomas.proisl@fau.de>
2016-09-01 09:03:16 +0000

    Summary: Versionsnummer
    git-svn-id: file:///data/svntogit/SoMaJo@32 cd5707a9-0dd3-4d93-bc9c-d03ad32e05cb

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.txt')) as fh:
 
 setup(
     name='SoMaJo',
-    version='1.1.1',
+    version='1.1.2',
     author='Thomas Proisl, Peter Uhrig',
     author_email='thomas.proisl@fau.de',
     packages=[
