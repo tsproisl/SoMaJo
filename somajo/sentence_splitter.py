9ae0fdcb2d5fc38bf057ab6a46282421cc66fc54
Thomas Proisl <thomas.proisl@fau.de>
2017-07-13 13:35:21 +0200

    Implemented sentence splitting

--- somajo/sentence_splitter.py
+++ somajo/sentence_splitter.py
@@ -2,3 +2,41 @@
 
 import regex as re
 
+
+class SentenceSplitter(object):
+    def __init__(self, is_tuple=False):
+        """Create a SentenceSplitter object. If the tokenized paragraphs
+        contain token classes or extra info, set is_tuple=True.
+
+        """
+        self.is_tuple = is_tuple
+        # full stop, ellipsis, exclamation and question marks
+        self.sentence_ending_punct = re.compile(r"^(?:\.+|…+\.*|[!?]+)$")
+        self.opening_punct = re.compile(r"^(?:['\"¿¡\p{Pi}\p{Ps}–—]|-{2,})$")
+        self.closing_punct = re.compile(r"^(?:['\"“\p{Pf}\p{Pe}])$")
+
+    def split(self, tokenized_paragraph):
+        """Split tokenized_paragraph into sentences."""
+        sentence_boundaries = []
+        paragraph_length = len(tokenized_paragraph)
+        tokens = tokenized_paragraph
+        # closing* opening* upper
+        if self.is_tuple:
+            tokens = [t[0] for t in tokenized_paragraph]
+        for i, token in enumerate(tokens):
+            if self.sentence_ending_punct.search(token):
+                last = None
+                boundary = i + 1
+                for j in range(i + 1, paragraph_length):
+                    token_j = tokens[j]
+                    if token_j[0].isupper():
+                        sentence_boundaries.append(boundary)
+                        break
+                    elif self.opening_punct.search(token_j):
+                        last = "opening"
+                    elif self.closing_punct.search(token_j) and last != "opening":
+                        boundary = j + 1
+                        last = "closing"
+                    else:
+                        break
+        return [tokenized_paragraph[i:j] for i, j in zip([0] + sentence_boundaries, sentence_boundaries + [paragraph_length])]
