b3968bd9830bbbde0b08610492e1fe652d4d847f
Thomas Proisl <thomas.proisl@fau.de>
2017-08-03 15:07:37 +0200

    Test cases for sentence splitting

---
+++ somajo/test/test_sentence_splitter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from somajo import SentenceSplitter
+from somajo import Tokenizer
+
+
+class TestSentenceSplitter(unittest.TestCase):
+    """"""
+    def setUp(self):
+        """Necessary preparations"""
+        self.tokenizer = Tokenizer(split_camel_case=True)
+        self.sentence_splitter = SentenceSplitter()
+
+    def _equal(self, raw, tokenized_sentences):
+        """"""
+        tokens = self.tokenizer.tokenize(raw)
+        sentences = self.sentence_splitter.split(tokens)
+        sentences = [" ".join(s) for s in sentences]
+        self.assertEqual(sentences, tokenized_sentences)
+
+
+class TestMisc(TestSentenceSplitter):
+    """"""
+    def test_misc_01(self):
+        self._equal("„Ich habe heute keine Zeit“, sagte die Frau und flüsterte leise: „Und auch keine Lust.“ Wir haben 1.000.000 Euro.", ["„ Ich habe heute keine Zeit “ , sagte die Frau und flüsterte leise : „ Und auch keine Lust . “", "Wir haben 1.000.000 Euro ."])
+
+    def test_misc_02(self):
+        self._equal("Es gibt jedoch einige Vorsichtsmaßnahmen, die Du ergreifen kannst, z. B. ist es sehr empfehlenswert, dass Du Dein Zuhause von allem Junkfood befreist.", ["Es gibt jedoch einige Vorsichtsmaßnahmen , die Du ergreifen kannst , z. B. ist es sehr empfehlenswert , dass Du Dein Zuhause von allem Junkfood befreist ."])
+
+    def test_misc_03(self):
+        self._equal("Was sind die Konsequenzen der Abstimmung vom 12. Juni?", ["Was sind die Konsequenzen der Abstimmung vom 12. Juni ?"])
