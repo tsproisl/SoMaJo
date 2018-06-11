ff90a908257d211ba060c20b983d323f96c42f91
Thomas Proisl <thomas.proisl@fau.de>
2018-06-11 10:49:04 +0200

    Added evaluation script for English Web Treebank

---
+++ utils/evaluate_on_ewt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPTDIR=$(dirname $BASH_SOURCE)
+cd $SCRIPTDIR
+
+mkdir tmp
+for f in ../data/English_Web_Treebank/en-ud-*.txt
+do
+    filename=$(basename $f)
+    ../bin/somajo-tokenizer -l en $f > tmp/$filename
+done
+echo "GOLD"
+perl ../data/empirist_gold_standard/tools/compare_tokenization.perl -e errors_test.txt tmp ../data/English_Web_Treebank/gold
+echo ""
+echo "SEMIGOLD"
+perl ../data/empirist_gold_standard/tools/compare_tokenization.perl -e errors_test.txt tmp ../data/English_Web_Treebank/semigold
+rm -r tmp/
