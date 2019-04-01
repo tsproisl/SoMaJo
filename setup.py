5bd21f840573987bce28ca4454f75aa0367b2d80
Thomas Proisl <thomas.proisl@fau.de>
2019-04-01 20:55:11 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as fh:
     long_description = fh.read()
 
-version = "1.8.3"
+version = "1.9.0"
 
 setup(
     name='SoMaJo',
