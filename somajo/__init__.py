4f85a692e7bbd2ae109c960df6348993ecdb34f8
Thomas Proisl <thomas.proisl@fau.de>
2019-07-02 06:39:11 +0200

    Bump version number

--- somajo/__init__.py
+++ somajo/__init__.py
@@ -1,7 +1,7 @@
 from somajo import tokenizer
 from somajo import sentence_splitter
 
-__version__ = "1.10.0"
+__version__ = "1.10.1"
 
 Tokenizer = tokenizer.Tokenizer
 SentenceSplitter = sentence_splitter.SentenceSplitter
