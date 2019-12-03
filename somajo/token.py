6c4e0be83a4f36080d15b44586d788f5472f4210
Thomas Proisl <thomas.proisl@fau.de>
2019-12-03 21:38:14 +0100

    Added token_class to __init__()

--- somajo/token.py
+++ somajo/token.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
+
 class Token:
-    def __init__(self, text, markup=False, locked=False, first_in_sentence=False, last_in_sentence=False):
+    def __init__(self, text, markup=False, locked=False, token_class=None, first_in_sentence=False, last_in_sentence=False):
         self.text = text
         self.markup = markup
         self.locked = locked
         self.first_in_sentence = first_in_sentence
         self.last_in_sentence = last_in_sentence
-        self.token_class = None
+        self.token_class = token_class
 
     def __str__(self):
         return self.text
