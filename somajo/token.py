29749fe25aa040455288d4cf9188bb9dd02c76e1
Thomas Proisl <thomas.proisl@fau.de>
2019-12-09 13:53:50 +0100

    Added attribute markup_class with possible values "start" and "end"

--- somajo/token.py
+++ somajo/token.py
@@ -2,9 +2,15 @@
 
 
 class Token:
-    def __init__(self, text, markup=False, locked=False, token_class=None, space_after=True, original_spelling=None, first_in_sentence=False, last_in_sentence=False):
+    def __init__(self, text, *, markup=False, markup_class=None, locked=False, token_class=None, space_after=True, original_spelling=None, first_in_sentence=False, last_in_sentence=False):
         self.text = text
+        if markup:
+            assert markup_class is not None
+        if markup_class is not None:
+            assert markup
+            assert markup_class == "start" or markup_class == "end"
         self.markup = markup
+        self.markup_class = markup_class
         self.locked = locked
         self.token_class = token_class
         self.space_after = space_after
