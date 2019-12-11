33b13233936b47918e363dd481fa18a74fcaa4cd
Thomas Proisl <thomas.proisl@fau.de>
2019-12-11 13:56:24 +0100

    Added documentation

--- somajo/token.py
+++ somajo/token.py
@@ -2,6 +2,30 @@
 
 
 class Token:
+    """Token objects store a piece of text (in the end a single token) with additional information.
+
+    Parameters
+    ----------
+    text : str
+        The text that makes up the token object
+    markup : bool
+        Is the token a markup token? (Default: False)
+    markup_class : str
+        If `markup=True`, then `markup_class` must be either "start" or "end". (Default: None)
+    locked : bool
+        Mark the token as locked. (Default: False)
+    token_class : str
+        The class of the token, e.g. "regular", "emoticon", "url", etc. (Default: None)
+    space_after : bool
+        Was there a space after the token in the original data? (Default: True)
+    original_spelling : str
+        The original spelling of the token, if it is different from the one in `text`. (Default: None)
+    first_in_sentence : bool
+        Is it the first token of a sentence? (Default: False)
+    last_in_sentence : bool
+        Is it the last token of a sentence? (Default: False)
+
+    """
     def __init__(self, text, *, markup=False, markup_class=None, locked=False, token_class=None, space_after=True, original_spelling=None, first_in_sentence=False, last_in_sentence=False):
         self.text = text
         if markup:
@@ -22,6 +46,13 @@ class Token:
         return self.text
 
     def extra_info(self):
+        """String representation of extra information.
+
+        Returns
+        -------
+        str
+            A string representation of the `space_after` and `original_spelling` attributes.
+        """
         info = []
         if not self.space_after:
             info.append("SpaceAfter=No")
