1205c79a6d59140c4f99074c0773187a1cc0a9b4
Thomas Proisl <thomas.proisl@fau.de>
2020-04-09 16:16:16 +0200

    Additional test cases for sentence splitting

--- somajo/test/test_sentence_splitter.py
+++ somajo/test/test_sentence_splitter.py
@@ -27,6 +27,13 @@ class TestSentenceSplitter(unittest.TestCase):
         self.assertEqual(sentences, tokenized_sentences)
 
 
+class TestSentenceSplitterEnglish(TestSentenceSplitter):
+    """"""
+    def setUp(self):
+        """Necessary preparations"""
+        self.tokenizer = SoMaJo("en_PTB", split_camel_case=True, split_sentences=True)
+
+
 class TestSentenceSplitterPretokenized(unittest.TestCase):
     """"""
     def setUp(self):
@@ -60,6 +67,57 @@ class TestMisc(TestSentenceSplitter):
     def test_misc_04(self):
         self._equal("Wir könnten wandern, schwimmen, Fahrrad fahren, usw. Worauf hättest du denn Lust?", ["Wir könnten wandern , schwimmen , Fahrrad fahren , usw.", "Worauf hättest du denn Lust ?"])
 
+    def test_misc_05(self):
+        self._equal("Er sagte: \"Das ist schlimm.\" Und damit hatte er recht.", ["Er sagte : \" Das ist schlimm . \"", "Und damit hatte er recht ."])
+
+    def test_misc_06(self):
+        self._equal("Er sagte: „Das ist schlimm.“ Und damit hatte er recht.", ["Er sagte : „ Das ist schlimm . “", "Und damit hatte er recht ."])
+
+    def test_misc_07(self):
+        self._equal("Foo \"Bar.\" Baz.", ["Foo \" Bar . \"", "Baz ."])
+
+    def test_misc_08(self):
+        self._equal("Foo „Bar.“ Baz.", ["Foo „ Bar . “", "Baz ."])
+
+    def test_misc_09(self):
+        self._equal("Foo “Bar.” Baz.", ["Foo “ Bar . ”", "Baz ."])
+
+    def test_misc_10(self):
+        self._equal("Foo »Bar.« Baz.", ["Foo » Bar . «", "Baz ."])
+
+    def test_misc_11(self):
+        self._equal("Foo «Bar.» Baz.", ["Foo « Bar . »", "Baz ."])
+
+    def test_misc_12(self):
+        self._equal("Foo (bar, baz, usw.) quux", ["Foo ( bar , baz , usw. ) quux"])
+
+    def test_misc_13(self):
+        self._equal("(Bar, baz, usw.) Quux", ["( Bar , baz , usw. )", "Quux"])
+
+    def test_misc_14(self):
+        self._equal("Foo, bar, baz, usw. quux", ["Foo , bar , baz , usw. quux"])
+
+    def test_misc_15(self):
+        self._equal("Foo, bar, baz, usw. Quux", ["Foo , bar , baz , usw.", "Quux"])
+
+    def test_misc_16(self):
+        self._equal("„Hi!“ „Hi!“", ["„ Hi ! “", "„ Hi ! “"])
+
+    def test_misc_17(self):
+        self._equal("blafasel bla. 700 Jahre später…", ["blafasel bla .", "700 Jahre später …"])
+
+
+class TestMiscEnglish(TestSentenceSplitterEnglish):
+    """"""
+    def test_misc_en_01(self):
+        self._equal("Foo \"Bar.\" Baz.", ["Foo \" Bar . \"", "Baz ."])
+
+    def test_misc_en_02(self):
+        self._equal("Foo “Bar.” Baz.", ["Foo “ Bar . ”", "Baz ."])
+
+    def test_misc_en_03(self):
+        self._equal("Foo «Bar.» Baz.", ["Foo « Bar . »", "Baz ."])
+
 
 class TestXML(TestSentenceSplitter):
     """"""
@@ -85,6 +143,9 @@ class TestXML(TestSentenceSplitter):
 
 class TestMiscPretokenized(TestSentenceSplitterPretokenized):
     """"""
+    # Successful disambiguation of “ requires full, i.e. not
+    # pre-tokenized, input text
+    @unittest.expectedFailure
     def test_misc_01(self):
         self._equal("„ Ich habe heute keine Zeit “ , sagte die Frau und flüsterte leise : „ Und auch keine Lust . “ Wir haben 1.000.000 Euro .", ["„ Ich habe heute keine Zeit “ , sagte die Frau und flüsterte leise : „ Und auch keine Lust . “", "Wir haben 1.000.000 Euro ."])
 
