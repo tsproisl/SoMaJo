86a11cf65d74b00fae5e0e4ef49317d63b39a6d9
Thomas Proisl <thomas.proisl@fau.de>
2020-04-17 09:35:05 +0200

    Removed old unused code

--- somajo/utils.py
+++ somajo/utils.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
-import collections
-import logging
 import os
 import regex as re
-import xml.etree.ElementTree as ET
 import xml.sax
 
 from somajo import doubly_linked_list
@@ -53,38 +50,6 @@ def read_abbreviation_file(filename):
                 continue
             abbreviations.add(line)
     return sorted(abbreviations, key=len, reverse=True)
-
-
-def parse_xml(xml, is_file=True):
-    """Return a list of XML elements and their text/tail as well as the
-    whole text of the document.
-
-    """
-    Element = collections.namedtuple("Element", ["element", "type", "text"])
-
-    def text_getter(elem):
-        text = elem.text
-        tail = elem.tail
-        if text is None:
-            text = ""
-        if tail is None:
-            tail = ""
-        yield Element(elem, "text", text)
-        for child in elem:
-            for t in text_getter(child):
-                yield t
-        yield Element(elem, "tail", tail)
-    try:
-        if is_file:
-            tree = ET.parse(xml)
-            root = tree.getroot()
-        else:
-            root = ET.fromstring(xml)
-    except ET.ParseError as err:
-        logging.error("Error parsing the XML file:\n%s" % err)
-        return []
-    elements = list(text_getter(root))
-    return elements
 
 
 class SaxTokenHandler(xml.sax.handler.ContentHandler):
